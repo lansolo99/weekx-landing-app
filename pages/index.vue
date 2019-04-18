d54cc69b62db2e7239966cc424de97f39df483b7
lan <lansolo99@hotmail.fr>
2019-04-18 08:03:56 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container class="yellow">
+    <v-layout>
+      <v-flex>Test</v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    // Logo,
+    // VuetifyLogo
+  }
+}
+</script>
