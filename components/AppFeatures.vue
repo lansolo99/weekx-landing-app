bf33ce3733d9dea0037827b57419e28ac54f9fcc
lan <lansolo99@hotmail.fr>
2019-04-21 23:34:19 +0200

    Continue implementing features & setup mixin breakpoints

--- components/AppFeatures.vue
+++ components/AppFeatures.vue
@@ -1,8 +1,16 @@
 <template>
   <section class="appFeatures">
     <v-container>
-      <v-layout>
-        <v-flex md4 class="red pa-4 appFeatures__column">
+      <v-layout wrap>
+        <v-flex
+          sm12
+          md4
+          class="red pa-5 appFeatures__column"
+          :class="{
+            'pa-5': $vuetify.breakpoint.smAndDown,
+            'pa-2': $vuetify.breakpoint.mdAndUp
+          }"
+        >
           <img
             src="~assets/images/appFeatures_habits.svg"
             alt="Create your habits"
@@ -13,11 +21,43 @@
             times a week, specific days or single tasks).
           </p>
         </v-flex>
-        <v-flex md4 class="green">
-          col
+        <v-flex
+          sm12
+          md4
+          class="red pa-5 appFeatures__column"
+          :class="{
+            'pa-5': $vuetify.breakpoint.smAndDown,
+            'pa-2': $vuetify.breakpoint.mdAndUp
+          }"
+        >
+          <img
+            src="~assets/images/appFeatures_progression.svg"
+            alt="Track your progression"
+          />
+          <h1 class="">Track your progression</h1>
+          <p>
+            Level-up your commitment to 100% on a 10 weeks range. Completion
+            records are also available by-task.
+          </p>
         </v-flex>
-        <v-flex md4 class="blue">
-          col
+        <v-flex
+          sm12
+          md4
+          class="red pa-5 appFeatures__column"
+          :class="{
+            'pa-5': $vuetify.breakpoint.smAndDown,
+            'pa-2': $vuetify.breakpoint.mdAndUp
+          }"
+        >
+          <img
+            src="~assets/images/appFeatures_reminder.svg"
+            alt="Set a daily reminder"
+          />
+          <h1 class="">Set a daily reminder</h1>
+          <p>
+            Activate notifications for your task(s) due today and choose a time
+            that fit your schedule.
+          </p>
         </v-flex>
       </v-layout>
     </v-container>
@@ -42,10 +82,16 @@ export default {}
     }
 
     h1 {
+      margin-top: 10px;
       font-size: 25px;
       font-weight: 500;
     }
     p {
+      @include responsive(tablet) {
+        padding-left: 4rem;
+        padding-right: 4rem;
+      }
+      margin-top: 20px;
       font-weight: 300;
       font-size: 18px;
     }
