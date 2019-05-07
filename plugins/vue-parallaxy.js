e109c65787d2f2810d198a180ccbb3222e93ec27
lan <lansolo99@hotmail.fr>
2019-05-07 17:50:50 +0200

    Optimize video size

--- plugins/vue-parallaxy.js
+++ plugins/vue-parallaxy.js
@@ -1,10 +1,3 @@
 import Vue from 'vue'
 import parallax from 'vue-parallaxy'
 Vue.use(parallax)
-
-// if (process.browser) {
-//   window.onNuxtReady(() => {
-//     const Parallax = require('vue-parallaxy')
-//     Vue.use(Parallax)
-//   })
-// }
